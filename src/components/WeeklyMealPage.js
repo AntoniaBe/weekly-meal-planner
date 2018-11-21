98fc01d23c882f0f775ec7d85484c1d6bec80bb2
Antonia Berger <AntoniaBerger@gmx.net>
2018-11-21 23:41:31 +0100

    worked on design, not finished

--- src/components/WeeklyMealPage.js
+++ src/components/WeeklyMealPage.js
@@ -1,42 +1,63 @@
 import React, { Component } from 'react';
 import '../style/WeeklyMealPage.css';
+import * as moment from 'moment';
+require('typeface-dancing-script');
 
 class WeeklyMealPage extends Component {
 
 
   render() {
+    const mealType= ['Breakfast', 'Lunch', 'Dinner', 'Snacks'];
+     const startOfWeek = moment().startOf('isoWeek');
+     const endOfWeek = moment().endOf('isoWeek');
 
-    let currentDate= new Date();
+     const days = [];
+     let day = startOfWeek;
 
-    return (
-      <div>
-      <div className="weekdayTitle">
-      <p>Monday</p>
-      {currentDate.getDate()}
-      </div>
-      <div className="weekdayTitle">
-      <p>Tuesday</p>
-      </div>
-      <div className="weekdayTitle">
-      <p>Wednesday</p>
-      </div>
-      <div className="weekdayTitle">
-      <p>Thursday</p>
-      </div>
-      <div className="weekdayTitle">
-      <p>Friday</p>
-      </div>
-      <div className="weekdayTitle">
-      <p>Saturday</p>
-      </div>
-      <div className="weekdayTitle">
-      <p>Sunday</p>
-      </div>
+     while (day <= endOfWeek) {
+       days.push({day: day.format('dddd').toString(), date:day.format("Do MMMM YYYY").toString(), });
+       day = day.clone().add(1, 'd');
+     }
+     console.log(days);
 
-      Hello
-      </div>
-    );
+
+  return(
+    <div className="container">
+    <ul className="meal-types">
+  {
+    mealType.map(mealType => <li className="subheader" key={mealType}>{mealType}</li>)
   }
+    </ul>
+
+    <div className='calendar'>
+      <div className='days-container'>
+        {days.map((data, idx) =>
+        <div className="days-container_day" key={idx}>
+          <h1 className="handschrift">{data.day}</h1>
+          <h2>{data.date}</h2>
+        </div>
+        )}
+      </div>
+      <div className='icon-grid'>
+        <ul>
+
+          <div className='add-meal-btn'>
+                  <button className="addButton">+</button>
+          </div>
+
+
+
+
+      </ul>
+
+</div>
+  </div>
+</div>
+  )
+
+  }
+
+
 }
 
 export default WeeklyMealPage;
