06b2763294f1091c16405ac477d399133980e4c4
Antonia Berger <AntoniaBerger@gmx.net>
2019-01-15 15:10:13 +0100

    added new api key and id

--- src/utils/apiCall.js
+++ src/utils/apiCall.js
@@ -1,6 +1,6 @@
 
 export function fetchRecipes (food = '') {
-  return fetch(`https://api.edamam.com/search?q=${food}&app_id=a0697a0d&app_key=f0946cf1ba1a71c0ed601acd6f6bc910&from=0&to=25`)
+  return fetch(`https://api.edamam.com/search?q=${food}&app_id=6af31f0f&app_key=9a49e659b05ab1956a3a9153d274a35e&from=0&to=25`)
     .then((res) => res.json())
     .then(({ hits }) => hits.map(({ recipe }) => recipe));
 }
